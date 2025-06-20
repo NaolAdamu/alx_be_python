cb08cc60262ac7b755689be38ae008d460ba6549
Naol Adamu <firaoladamu97@gmail.com>
2025-06-20 09:39:36 +0300

    simple python and simple interest operation

--- basic_operation.py
+++ basic_operation.py
@@ -2,4 +2,8 @@ number1 =10
 number2 =5
 answer1= number1+number2
 print (answer1)
+answer2= number1-number2
+print(answer2)
+answer3=number1*number2
+print(answer3)
 
---
+++ simple_interst.py
@@ -0,0 +1,6 @@
+principle=1000
+rate=0.05
+time=3
+interst=principle*rate*time
+print("the interest is:", interst)
+print 

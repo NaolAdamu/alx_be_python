5db35f25dacb3d9309a8cc5ae0bb23d271b87769
Naol Adamu <firaoladamu97@gmail.com>
2025-06-26 13:56:40 +0300

    Add weather_advice.py with Weather Recommendation

---
+++ weather_advice.py
@@ -0,0 +1,7 @@
+weather = input("What is the wather like today? (sunny/rainy/cold): ")
+if weather == "sunny":
+    print ("Wear a t-shirt and sunglassess.")
+elif weather == "rainy":
+    print ("Don't forget your umbrella and raincoat")
+elif weather =="cold":
+    print ("Sorry, I don't have recommendation for this weather. ")
